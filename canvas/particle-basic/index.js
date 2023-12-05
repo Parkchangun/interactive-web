149cbb831a0f10f84b537e7210cfe4c84e41badd
changeonPark <qkrcksrjs@gmail.com>
2023-12-05 10:44:54 +0900

    🚩 canvas 크기

--- canvas/particle-basic/index.js
+++ canvas/particle-basic/index.js
@@ -2,10 +2,31 @@ const canvas = document.querySelector("canvas")
 
 console.log(canvas)
 
-const ctx = canvas.getContext('2d')
+const ctx = canvas.getContext("2d")
 console.log(ctx)
 
-canvas.style.width = 300 + 'px'
-canvas.style.height = 300 + 'px'
+/**
+ * devicePixelRatio(DPR) - 하나의 CSS를 픽셀을 그릴 때 사용되는 사용되는 장치의 픽셀 수
+ * ../assets/dpr.png
+ */
+
+const DPR = window.devicePixelRatio
+
+console.log(DPR)
+
+/**
+ * style과 canvas 고유의 사이즈를 동일하게 해 작업을 하는 것이 일반적
+ */
+
+const canvasWidth = 300
+const canvasHeight = 300
+
+canvas.width = canvasWidth * DPR
+canvas.height = canvasHeight * DPR
+// DPR이 2 이상인 기기에선 좀 더 선명하게 보임
+ctx.scale(DPR, DPR)
+
+canvas.style.width = canvasWidth + "px"
+canvas.style.height = canvasHeight + "px"
 
 ctx.fillRect(10, 10, 50, 50)
