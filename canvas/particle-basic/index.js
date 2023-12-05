75809e6fb5f5a700862ef37466bdf0ce3778a89c
changeonPark <qkrcksrjs@gmail.com>
2023-12-05 12:02:17 +0900

    🚩 feat: particle animation

--- canvas/particle-basic/index.js
+++ canvas/particle-basic/index.js
@@ -18,8 +18,8 @@ console.log(DPR)
  * style과 canvas 고유의 사이즈를 동일하게 해 작업을 하는 것이 일반적
  */
 
-const canvasWidth = 300
-const canvasHeight = 300
+const canvasWidth = innerWidth
+const canvasHeight = innerHeight
 
 canvas.width = canvasWidth * DPR
 canvas.height = canvasHeight * DPR
@@ -31,10 +31,11 @@ canvas.style.height = canvasHeight + "px"
 
 const RADIAN = Math.PI / 180
 class Particle {
-  constructor(x, y, radius) {
+  constructor(x, y, radius, vy) {
     this.x = x
     this.y = y
     this.radius = radius
+    this.vy = vy
   }
 
   draw() {
@@ -43,19 +44,57 @@ class Particle {
     // radian을 사용하므로
 
     ctx.arc(this.x, this.y, this.radius, 0, RADIAN * 360)
-    ctx.fillStyle = "red"
+    ctx.fillStyle = "orange"
     ctx.fill()
     ctx.closePath()
   }
+
+  update() {
+    // this.y += Math.cos(Date.now() / 1000) * 5
+    this.y += this.vy
+  }
 }
 
-const particle = new Particle(100, 100, 50)
+const randomNumberBetween = (min, max) => {
+  return Math.random() * (max - min + 1) + min
+}
+
+const particles = new Array(50).fill(0).map(() => {
+  const x = randomNumberBetween(0, canvasWidth)
+  const y = randomNumberBetween(0, canvasHeight)
+  const radius = randomNumberBetween(50, 100)
+  const vy = randomNumberBetween(1, 5)
+
+  return new Particle(x, y, radius, vy)
+})
+
+// 60fps
+let interval = 1000 / 60
+let now, delta
+let then = Date.now()
 
 function animate() {
-  ctx.clearRect(0, 0, canvasWidth, canvasHeight)
-  particle.draw()
-
   window.requestAnimationFrame(animate)
+
+  now = Date.now()
+  delta = now - then
+
+  if (delta < interval) return
+
+  ctx.clearRect(0, 0, canvasWidth, canvasHeight)
+
+  particles.forEach((particle) => {
+    particle.update()
+    particle.draw()
+    if (particle.y - particle.radius > canvasHeight) {
+      particle.x = randomNumberBetween(0, canvasWidth)
+      particle.y = -particle.radius
+      particle.radius = randomNumberBetween(50, 100)
+      particle.vy = randomNumberBetween(1, 5)
+    }
+  })
+
+  then = now - (delta & interval)
 }
 
-// animate()
+animate()
